9e52428330d68fe8f71fb944f14c01f5ce976a14
RichenelWartim <114988676+RichenelWartim@users.noreply.github.com>
2023-02-09 12:54:47 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Homepage</title>
+</head>
+
+<body>
+    </nav>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="w-50 p-3 bg-info" style="height: 200px">
+                    <h1>Richenel Wartim Dashboard</h1>
+                    <h4>Hier Kunt u zien allen dingen die ik heb gemaakt tot nu toe. Voor vragen Kom naar me toe en stel ze </h4>
+                </div>
+            </div>
+            <div class="col-sm-3 w-50">
+                <a href="Galgje/" target="_blank"><img src="img/Galgje.png" alt="HTML tutorial" class="row-sm-4 w-50" style="width: 100%;"></a>
+            </div>
+            <div class="col-sm-4 w-50">
+                <div class="row-sm-3 " style="height: 425px;">
+                    <a href="RPS/" target="_blank"><img src="img/RPS.png" alt="HTML tutorial" class="row-sm-4 w-50" style="width: 100%;"></a>
+                </div>
+            </div>
+            <div class="col-sm-4 w-50">
+                <div class="row-sm-3 " style="height: 425px;">
+                    <a href="Wie/" target="_blank"><img src="img/Wie.png" alt="HTML tutorial" class="row-sm-4 w-50" style="width: 100%;"></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 w-50">
+            <div class="row-sm-3 " style="height: 425px;">
+                <a href="br/" target="_blank"><img src="img/br.png" alt="HTML tutorial" class="row-sm-4 w-50" style="width: 100%;"></a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    </div>
+</body>
+
+</html>
